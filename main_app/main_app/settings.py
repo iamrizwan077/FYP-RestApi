13574fe566854e139c7ffcb388c1aa9d50507f4a
iamrizwan077 <77151292+iamrizwan077@users.noreply.github.com>
2024-03-06 12:55:45 +0000

    Pending changes exported from your codespace

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -65,6 +66,35 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware"
 ]
+
+# JAZZMIN_SETTINGS = {
+#     # title of the window (Will default to current_admin_site.site_title if absent or None)
+#     "site_title": "Library Admin",
+
+#     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_header": "Library",
+
+#     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_brand": "Library",
+
+#     # Logo to use for your site, must be present in static files, used for brand on top left
+#     "site_logo": "books/img/logo.png",
+
+#     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+#     "login_logo": None,
+
+#     # Logo to use for login form in dark themes (defaults to login_logo)
+#     "login_logo_dark": None,
+
+#     # CSS classes that are applied to the logo above
+#     "site_logo_classes": "img-circle",
+
+#     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+#     "site_icon": None,
+
+#     # Welcome text on the login screen
+#     "welcome_sign": "Welcome to the library",
+# }
 
 ROOT_URLCONF = "main_app.urls"
 
@@ -147,7 +177,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- main_app/myapp/models.py
+++ main_app/myapp/models.py
@@ -6,10 +6,24 @@ class NTN(models.Model):
     ntn = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=256)
     location = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return f'{self.ntn} - {self.name}'
+    
+    class Meta:
+        verbose_name = ("NTN")
+        verbose_name_plural = ("NTNs")
     
 class AnomalyInfo(models.Model):
     id = models.IntegerField(primary_key=True)
     description=models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return f'{self.id} - {self.description}'
+    
+    class Meta:
+        verbose_name = ("Anomaly Info")
+        verbose_name_plural = ("Anomaly Infos")
 
 # class POS(models.Model):
 #     pos = models.IntegerField()
@@ -27,13 +41,13 @@ class Anomaly(models.Model):
     pos_id = models.IntegerField()
     ntn = models.ForeignKey(NTN, on_delete=models.DO_NOTHING)
     invoice_date = models.DateTimeField()
-    invoice_no = models.CharField(max_length=256)
-    rate_value = models.FloatField()
+    invoice_no = models.CharField(max_length=256, null=True)
+    rate_value = models.FloatField(null=True)
     sales_value = models.FloatField()
     sales_tax = models.FloatField()
     consumer_name = models.CharField(max_length=256, null=True)
     # consumer_ntn = models.ForeignKey(NTN, on_delete=models.DO_NOTHING)
-    consumer_ntn = models.IntegerField(null=True)
+    consumer_ntn = models.CharField(null=True, max_length=256, blank=True)
     consumer_address = models.CharField(max_length=256, null=True)
     tariff_code = models.CharField(null=True, max_length=256, blank=True)
     extra_info = models.CharField(max_length=256, null=True)
@@ -41,13 +55,23 @@ class Anomaly(models.Model):
     pos_pass = models.CharField(max_length=256, null=True)
     is_active =models.BooleanField()
     created_date_time = models.DateTimeField()
-    invoice_type = models.IntegerField()
+    invoice_type = models.CharField(null=True, max_length=256, blank=True)
     consider_for_annex = models.IntegerField(null=True)
     anomaly = models.ForeignKey(AnomalyInfo, on_delete=models.PROTECT, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.srb_invoice_id} - {self.anomaly}'
     # anomaly = models.IntegerField(null=True)
+    class Meta:
+        verbose_name = ("Anomaly")
+        verbose_name_plural = ("Anomalies")
 
 class MissingInvoice(models.Model):
     ntn = models.ForeignKey(NTN, on_delete=models.DO_NOTHING)
     invoices = models.CharField(validators=[int_list_validator], max_length=256)
     date = models.DateField()
+
+    class Meta:
+        verbose_name = ("Missing Invoice")
+        verbose_name_plural = ("Missing Invoices")
 
